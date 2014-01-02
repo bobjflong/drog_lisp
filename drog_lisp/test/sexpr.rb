0683e2ba4221e8f91373a9ed4ee42764f0674983
Bob Long <robertjflong@gmail.com>
2014-01-02 10:27:22 +0000

    nicer homoiconicity example

--- drog_lisp/test/sexpr.rb
+++ drog_lisp/test/sexpr.rb
@@ -142,13 +142,13 @@ describe "S-Expression extraction" do
   it "Allows list literals to be built" do
     prog = %Q(
     (Do
-      (Show (Evaluate (list :+ 2 (:+ 7 3))))
+      (Show (Evaluate (list (:+ 2 (:+ 7 3)))))
     )
     )
 
     list_literal = LispMacro.new 'list' do |ast|
-      elems = ast.drop(1)
-      elems.to_cons 
+      elems = ast.drop(1).flatten 1
+      elems.to_cons
     end
 
     LispPreprocessor.preprocess prog, MacroList.new([list_literal])
